2b9155e7f44cc664aa7a8751b16aa88efbb94e62
Diego Godoy <diegoalex-gdy@outlook.com>
2025-09-09 16:59:49 -0300

    Remove obsolete register_template_extensions call - NetBox 4.4 auto-registers from template_content.py

--- netbox_maintenance_device/__init__.py
+++ netbox_maintenance_device/__init__.py
@@ -6,15 +6,5 @@ class MaintenanceDeviceConfig(PluginConfig):
     description = 'Manage device preventive and corrective maintenance'
     version = '1.0.0'
     base_url = 'maintenance-device'
-    
-    def ready(self):
-        super().ready()
-        
-        # Register device tab
-        from dcim.models import Device
-        from netbox.plugins.utils import register_template_extensions
-        from .template_content import DeviceMaintenanceTab
-        
-        register_template_extensions([DeviceMaintenanceTab])
 
 config = MaintenanceDeviceConfig
