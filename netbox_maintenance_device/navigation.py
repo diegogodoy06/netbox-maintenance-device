b996ee5138a843a39b34151c11642557e7914637
Diego Godoy <diegoalex-gdy@outlook.com>
2025-09-10 11:20:12 +0000

    Implement major NetBox maintenance plugin enhancements: device tab, Portuguese-BR support, enhanced overdue UI, and quick completion functionality
    Co-authored-by: diegogodoy06 <85560773+diegogodoy06@users.noreply.github.com>

--- netbox_maintenance_device/navigation.py
+++ netbox_maintenance_device/navigation.py
@@ -1,19 +1,20 @@
 from netbox.plugins import PluginMenuItem, PluginMenuButton
+from django.utils.translation import gettext_lazy as _
 
 menu_items = (
     PluginMenuItem(
         link='plugins:netbox_maintenance_device:upcoming_maintenance',
-        link_text='Upcoming Maintenance',
+        link_text=_('Upcoming Maintenance'),
         permissions=['netbox_maintenance_device.view_maintenanceplan']
     ),
     PluginMenuItem(
         link='plugins:netbox_maintenance_device:maintenanceplan_list',
-        link_text='Maintenance Plans',
+        link_text=_('Maintenance Plans'),
         permissions=['netbox_maintenance_device.view_maintenanceplan'],
         buttons=(
             PluginMenuButton(
                 link='plugins:netbox_maintenance_device:maintenanceplan_add',
-                title='Add',
+                title=_('Add'),
                 icon_class='mdi mdi-plus-thick',
                 permissions=['netbox_maintenance_device.add_maintenanceplan']
             ),
@@ -21,12 +22,12 @@ menu_items = (
     ),
     PluginMenuItem(
         link='plugins:netbox_maintenance_device:maintenanceexecution_list',
-        link_text='Maintenance Executions',
+        link_text=_('Maintenance Executions'),
         permissions=['netbox_maintenance_device.view_maintenanceexecution'],
         buttons=(
             PluginMenuButton(
                 link='plugins:netbox_maintenance_device:maintenanceexecution_add',
-                title='Add',
+                title=_('Add'),
                 icon_class='mdi mdi-plus-thick',
                 permissions=['netbox_maintenance_device.add_maintenanceexecution']
             ),
--- netbox_maintenance_device/template_content.py
+++ netbox_maintenance_device/template_content.py
@@ -1,10 +1,41 @@
 from netbox.plugins import PluginTemplateExtension
 from dcim.models import Device
+from . import models
 
 class DeviceMaintenanceTab(PluginTemplateExtension):
     models = ['dcim.device']
     
     def left_page(self):
         return self.render('netbox_maintenance_device/device_maintenance_tab.html')
+    
+    def buttons(self):
+        return self.render('netbox_maintenance_device/device_maintenance_buttons.html')
 
-template_extensions = [DeviceMaintenanceTab]
+class DeviceMaintenanceTabContent(PluginTemplateExtension):
+    models = ['dcim.device']
+    
+    def detail_tabs(self):
+        return [
+            {
+                'title': 'Maintenance',
+                'url': 'netbox_maintenance_device:device_maintenance_tab',
+                'permission': 'netbox_maintenance_device.view_maintenanceplan',
+                'badge': self._get_maintenance_badge(),
+                'hide_if_empty': False
+            }
+        ]
+    
+    def _get_maintenance_badge(self):
+        """Get badge count for overdue maintenance"""
+        if hasattr(self, 'context') and 'object' in self.context:
+            device = self.context['object']
+            overdue_count = models.MaintenancePlan.objects.filter(
+                device=device, 
+                is_active=True
+            ).filter(
+                # Get plans where next maintenance date is in the past
+            ).count()
+            return overdue_count if overdue_count > 0 else None
+        return None
+
+template_extensions = [DeviceMaintenanceTab, DeviceMaintenanceTabContent]
