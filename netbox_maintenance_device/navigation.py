1ca8c5f6e1f7a3a753ec15acda6f141b028e3ef9
Diego Godoy <diegoalex-gdy@outlook.com>
2025-09-10 10:40:40 -0300

    fix: remove icon menu

--- netbox_maintenance_device/navigation.py
+++ netbox_maintenance_device/navigation.py
@@ -5,14 +5,12 @@ menu_items = (
     PluginMenuItem(
         link='plugins:netbox_maintenance_device:upcoming_maintenance',
         link_text=_('Upcoming Maintenance'),
-        permissions=['netbox_maintenance_device.view_maintenanceplan'],
-        icon_class='mdi-clock-alert-outline'
+        permissions=['netbox_maintenance_device.view_maintenanceplan']
     ),
     PluginMenuItem(
         link='plugins:netbox_maintenance_device:maintenanceplan_list',
         link_text=_('Maintenance Plans'),
         permissions=['netbox_maintenance_device.view_maintenanceplan'],
-        icon_class='mdi-calendar-check',
         buttons=(
             PluginMenuButton(
                 link='plugins:netbox_maintenance_device:maintenanceplan_add',
@@ -26,7 +24,6 @@ menu_items = (
         link='plugins:netbox_maintenance_device:maintenanceexecution_list',
         link_text=_('Maintenance Executions'),
         permissions=['netbox_maintenance_device.view_maintenanceexecution'],
-        icon_class='mdi-wrench',
         buttons=(
             PluginMenuButton(
                 link='plugins:netbox_maintenance_device:maintenanceexecution_add',
